d862ddff79ea1981486154c5ea9f2538bce70def
purplerez <fairy.edensor@gmail.com>
2025-05-05 14:46:54 +0700

    fix : remix, package

--- remix.config.js
+++ remix.config.js
@@ -1,12 +1,8 @@
 /** @type {import('@remix-run/dev').AppConfig} */
-module.exports = {
-    serverBuildTarget: "vercel", // Pastikan ini adalah 'vercel'
-    future: {
-      v3_fetcherPersist: true,
-      v3_lazyRouteDiscovery: true,
-      v3_relativeSplatPath: true,
-      v3_singleFetch: true,
-      v3_throwAbortReason: true,
-    },
-  };
-  
+export default {
+  serverBuildTarget: "vercel",
+  ignoredRouteFiles: ["**/.*"],
+  future: {
+    v3_routeConvention: true,
+  },
+};
